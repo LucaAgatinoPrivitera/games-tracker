b5b1337a1c658bf87278e793a08c1c9ed0359853
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-12-10 00:12:36 +0100

    Sistemato il layout, e cambiata la barra sopra che riporta l'user

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,22 @@
 // components/Navbar.tsx
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 interface NavbarProps {
-    className?: string; // Prop per le classi CSS opzionali
+	className?: string; // Prop per le classi CSS opzionali
 }
 
 const Navbar: React.FC<NavbarProps> = ({ className }) => {
-    return (
-        <div id="navbar" className={`bg-slate-950 w-full py-4 ${className}`}>
-            <div className="lg:container mx-auto flex justify-between items-center h-full">
-                <Link className="text-white hover:text-white shadow transition ease-in-out duration-300" to="/">
-                    <h1 className="oswald cursor-pointer">GamesTrackers</h1>
-                </Link>
-                <div className="flex align-middle gap-4 montserrat">
-                    <Link className="no-underline text-white hover:text-slate-950 hover:bg-white rounded transition ease-in-out duration-300 px-2 py-1" to="/register">
+	return (
+		<div id="navbar" className={`w-full py-4 ${className}`}>
+			<div className="lg:container mx-auto flex justify-between items-center h-full">
+				<Link
+					className="text-white hover:text-white shadow transition ease-in-out duration-300"
+					to="/"
+				>
+					<h1 className="oswald cursor-pointer">GamesTrackers</h1>
+				</Link>
+				<div className="flex align-middle gap-4 montserrat">
+					{/* <Link className="no-underline text-white hover:text-slate-950 hover:bg-white rounded transition ease-in-out duration-300 px-2 py-1" to="/register">
                         Registrati
                     </Link>
                     <Link className="no-underline text-white hover:text-slate-950 hover:bg-white rounded transition ease-in-out duration-300 px-2 py-1" to="/login">
@@ -21,11 +24,26 @@ const Navbar: React.FC<NavbarProps> = ({ className }) => {
                     </Link>
                     <Link className="no-underline text-white hover:text-slate-950 hover:bg-white rounded transition ease-in-out duration-300 px-2 py-1" to="/about">
                         About
-                    </Link>
-                </div>
-            </div>
-        </div>
-    );
+                    </Link> */}
+					<div className="flex flex-col content-center">
+						<img
+							id="picUser"
+							className="rounded-full mb-1 cursor-pointer"
+							src="/1_Lfp9i4K.webp"
+							alt=""
+						/>
+						<a
+							id="logUser"
+							className="cursor-pointer no-underline text-center mt-1"
+							href="#"
+						>
+							Luca
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
