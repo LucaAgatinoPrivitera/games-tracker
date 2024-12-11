971925cfaed35873e94344bbc0ee196fb3a6d6c1
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-12-11 10:42:14 +0100

    Aggiunto padding b

--- src/views/news.tsx
+++ src/views/news.tsx
@@ -1,164 +1,186 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 // Mi permette di usare le prop
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate } from "react-router-dom";
 
 interface NewsItem {
-    gid: string;
-    title: string;
-    url: string;
-    author: string;
-    contents: string;
-    name: string;
-    appid: number;
-    date: number;
+	gid: string;
+	title: string;
+	url: string;
+	author: string;
+	contents: string;
+	name: string;
+	appid: number;
+	date: number;
 }
 
 const GameNews = () => {
-    const { appid, name } = useParams<{ appid: string; name: string }>(); //Prende appid per aprire le news specifichi e il name per il nome appunto
-    const [news, setNews] = useState<NewsItem[]>([]);
-    const [error, setError] = useState<string | null>(null);
-    const [loading, setLoading] = useState<boolean>(true); // Stato di caricamento
-    const [currentPage, setCurrentPage] = useState(1);
-    const NewsPerPage = 10;
-    const navigate = useNavigate();
+	const { appid, name } = useParams<{ appid: string; name: string }>(); //Prende appid per aprire le news specifichi e il name per il nome appunto
+	const [news, setNews] = useState<NewsItem[]>([]);
+	const [error, setError] = useState<string | null>(null);
+	const [loading, setLoading] = useState<boolean>(true); // Stato di caricamento
+	const [currentPage, setCurrentPage] = useState(1);
+	const NewsPerPage = 10;
+	const navigate = useNavigate();
 
-    useEffect(() => {
-        document.title = `Notizie di ${name}`;
+	useEffect(() => {
+		document.title = `Notizie di ${name}`;
 
-        const fetchNewsForGame = async () => {
-            setLoading(true); // Inizia il caricamento
+		const fetchNewsForGame = async () => {
+			setLoading(true); // Inizia il caricamento
 
-            try {
-                const response = await fetch(
-                    `/api/ISteamNews/GetNewsForApp/v0002/?appid=${appid}&count=100&maxlength=300&format=json`
-                );
+			try {
+				const response = await fetch(
+					`/api/ISteamNews/GetNewsForApp/v0002/?appid=${appid}&count=100&maxlength=300&format=json`
+				);
 
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
+				if (!response.ok) {
+					throw new Error(`HTTP error! status: ${response.status}`);
+				}
 
-                const data = await response.json();
-                console.log(data);
-                setNews(data.appnews.newsitems);
-                setError(null); // Resetta l'errore se i dati vengono recuperati correttamente
-            } catch (error) {
-                setError((error as Error).message || 'Errore durante il recupero delle notizie');
-                setNews([]); // Resetta le notizie se c'è un errore
-            } finally {
-                setLoading(false); // Caricamento terminato
-            }
-        };
+				const data = await response.json();
+				console.log(data);
+				setNews(data.appnews.newsitems);
+				setError(null); // Resetta l'errore se i dati vengono recuperati correttamente
+			} catch (error) {
+				setError(
+					(error as Error).message || "Errore durante il recupero delle notizie"
+				);
+				setNews([]); // Resetta le notizie se c'è un errore
+			} finally {
+				setLoading(false); // Caricamento terminato
+			}
+		};
 
-        fetchNewsForGame();
-    }, [appid]);
+		fetchNewsForGame();
+	}, [appid]);
 
-    const extractImageUrl = (contents: string): string => {
-        const regex = /{STEAM_CLAN_IMAGE}\/(\d+)\/([a-zA-Z0-9]+\.png|[a-zA-Z0-9]+\.jpg|[a-zA-Z0-9]+\.jpeg|[a-zA-Z0-9]+\.webp)/;
-        const match = contents.match(regex);
+	const extractImageUrl = (contents: string): string => {
+		const regex =
+			/{STEAM_CLAN_IMAGE}\/(\d+)\/([a-zA-Z0-9]+\.png|[a-zA-Z0-9]+\.jpg|[a-zA-Z0-9]+\.jpeg|[a-zA-Z0-9]+\.webp)/;
+		const match = contents.match(regex);
 
-        if (match) {
-            return `https://clan.fastly.steamstatic.com/images/${match[1]}/${match[2]}`;
-        } else {
-            return 'https://www.lffl.org/wp-content/uploads/2022/02/valve-proton.jpg';
-        }
-    };
+		if (match) {
+			return `https://clan.fastly.steamstatic.com/images/${match[1]}/${match[2]}`;
+		} else {
+			return "https://www.lffl.org/wp-content/uploads/2022/02/valve-proton.jpg";
+		}
+	};
 
-    const formatDate = (timestamp: number) => {
-        const date = new Date(timestamp * 1000);
-        return date.toLocaleDateString('it-IT', {
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric',
-            hour: '2-digit',
-            minute: '2-digit',
-        });
-    };
+	const formatDate = (timestamp: number) => {
+		const date = new Date(timestamp * 1000);
+		return date.toLocaleDateString("it-IT", {
+			year: "numeric",
+			month: "long",
+			day: "numeric",
+			hour: "2-digit",
+			minute: "2-digit",
+		});
+	};
 
-    const formatDatePatchNotes = (timestamp: number) => {
-        const date = new Date(timestamp * 1000);
-        return date.toLocaleDateString('it-IT', {
-            year: 'numeric',
-            month: 'numeric',
-            day: 'numeric',
-        });
-    };
+	const formatDatePatchNotes = (timestamp: number) => {
+		const date = new Date(timestamp * 1000);
+		return date.toLocaleDateString("it-IT", {
+			year: "numeric",
+			month: "numeric",
+			day: "numeric",
+		});
+	};
 
-    const totalPages = Math.ceil(news.length / NewsPerPage);
-    const currentNews = news.slice((currentPage - 1) * NewsPerPage, currentPage * NewsPerPage);
+	const totalPages = Math.ceil(news.length / NewsPerPage);
+	const currentNews = news.slice(
+		(currentPage - 1) * NewsPerPage,
+		currentPage * NewsPerPage
+	);
 
-    useEffect(() => {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    }, [currentPage]);
+	useEffect(() => {
+		window.scrollTo({ top: 0, behavior: "smooth" });
+	}, [currentPage]);
 
-    return (
-        <div className='lg:container mx-auto pt-8'>
-            {/* Freccia per tornare indietro */}
-            <div className="flex gap-4">
-                <button onClick={() => navigate(-1)} className='mb-4 px-4 py-2 border text-white rounded border-0 back-shadow'>
-                    <i className="fa-solid fa-arrow-left"></i>
-                </button>
-                <h1 className='pb-4 font-bold'>Notizie di: {name}</h1>
-            </div>
+	return (
+		<div className="lg:container mx-auto pt-8">
+			{/* Freccia per tornare indietro */}
+			<div className="flex gap-4">
+				<button
+					onClick={() => navigate(-1)}
+					className="mb-4 px-4 py-2 border text-white rounded border-0 back-shadow"
+				>
+					<i className="fa-solid fa-arrow-left"></i>
+				</button>
+				<h1 className="pb-4 font-bold">Notizie di: {name}</h1>
+			</div>
 
-            {loading ? (
-                <div>
-                    <h1>Caricamento delle notizie in corso...</h1> {/* Indica che i dati stanno arrivando */}
-                </div>
-            ) : error ? (
-                <div>
-                    <h1>Errore 404</h1>
-                    <h1>{error}</h1> {/* Mostra il messaggio di errore */}
-                </div>
-            ) : news.length === 0 ? (
-                <div>
-                    <h1>Ops. Nessuna notizia disponibile per questo gioco.</h1>
-                </div>
-            ) : (
-                <div className='mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4'>
-                    {currentNews.map((newsItem, index) => {
-                        const imageUrl = extractImageUrl(newsItem.contents);
-                        const formattedDate = formatDate(newsItem.date);
-                        const formattedDatePatchNotes = formatDatePatchNotes(newsItem.date);
+			{loading ? (
+				<div>
+					<h1>Caricamento delle notizie in corso...</h1>{" "}
+					{/* Indica che i dati stanno arrivando */}
+				</div>
+			) : error ? (
+				<div>
+					<h1>Errore 404</h1>
+					<h1>{error}</h1> {/* Mostra il messaggio di errore */}
+				</div>
+			) : news.length === 0 ? (
+				<div>
+					<h1>Ops. Nessuna notizia disponibile per questo gioco.</h1>
+				</div>
+			) : (
+				<div className="mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
+					{currentNews.map((newsItem, index) => {
+						const imageUrl = extractImageUrl(newsItem.contents);
+						const formattedDate = formatDate(newsItem.date);
+						const formattedDatePatchNotes = formatDatePatchNotes(newsItem.date);
 
-                        return (
-                            <div key={index} className='flex flex-col mb-4'>
-                                {newsItem.title === 'Team Fortress 2 Update Released' ? (
-                                    <div className='flex'>
-                                        <h3 className='font-bold flex-1'>{newsItem.title}</h3>
-                                        <h3 className='ms-2 font-bold'>{formattedDatePatchNotes}</h3>
-                                    </div>
-                                ) : (
-                                    <h3 className='font-bold flex-1'>{newsItem.title}</h3>
-                                )}
+						return (
+							<div key={index} className="flex flex-col mb-4">
+								{newsItem.title === "Team Fortress 2 Update Released" ? (
+									<div className="flex">
+										<h3 className="font-bold flex-1">{newsItem.title}</h3>
+										<h3 className="ms-2 font-bold">
+											{formattedDatePatchNotes}
+										</h3>
+									</div>
+								) : (
+									<h3 className="font-bold flex-1">{newsItem.title}</h3>
+								)}
 
-                                <a onClick={() => window.open(`${newsItem.url}`, `_blank`)} className='text-blue-500 hover:underline cursor-pointer hover:no-underline'>
-                                    Per saperne di più
-                                </a>
+								<a
+									onClick={() => window.open(`${newsItem.url}`, `_blank`)}
+									className="text-blue-500 hover:underline cursor-pointer hover:no-underline"
+								>
+									Per saperne di più
+								</a>
 
-                                <a href={`${newsItem.url}`} className='text-blue-500 hover:underline block'>
-                                    <img src={imageUrl} alt={newsItem.title} className='mt-2 ' />
-                                </a>
-                                <p className=''>Autore: {newsItem.author || 'N/A'}</p>
-                                <p className='text-sm text-gray-500'>Data: {formattedDate}</p>
-                            </div>
-                        );
-                    })}
-                </div>
-            )}
+								<a
+									href={`${newsItem.url}`}
+									className="text-blue-500 hover:underline block"
+								>
+									<img src={imageUrl} alt={newsItem.title} className="mt-2 " />
+								</a>
+								<p className="">Autore: {newsItem.author || "N/A"}</p>
+								<p className="text-sm text-gray-500">Data: {formattedDate}</p>
+							</div>
+						);
+					})}
+				</div>
+			)}
 
-            <div className="flex justify-center mt-4 lg:container mx-auto overflow-hidden">
-                {Array.from({ length: totalPages }, (_, index) => (
-                    <button
-                        key={index + 1}
-                        className={`px-4 py-2 mx-1 border ${index + 1 === currentPage ? 'bg-gray-800 text-white' : 'bg-white text-gray-800'}`}
-                        onClick={() => setCurrentPage(index + 1)}
-                    >
-                        {index + 1}
-                    </button>
-                ))}
-            </div>
-        </div>
-    );
+			<div className="flex justify-center mt-4 pb-8 lg:container mx-auto overflow-hidden">
+				{Array.from({ length: totalPages }, (_, index) => (
+					<button
+						key={index + 1}
+						className={`px-4 py-2 mx-1 border ${
+							index + 1 === currentPage
+								? "bg-gray-800 text-white"
+								: "bg-white text-gray-800"
+						}`}
+						onClick={() => setCurrentPage(index + 1)}
+					>
+						{index + 1}
+					</button>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default GameNews;
--- src/views/ownedGames.tsx
+++ src/views/ownedGames.tsx
@@ -1,212 +1,219 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 // Mi permette di usare le props
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 interface OwnedGame {
-  appid: number;
-  name: string;
-  img_icon_url: string;
-  playtime_forever: number;
-  rtime_last_played: number;
+	appid: number;
+	name: string;
+	img_icon_url: string;
+	playtime_forever: number;
+	rtime_last_played: number;
 }
 
 const GameFeed = () => {
-  const [games, setGames] = useState<OwnedGame[]>([]);
-  const [error, setError] = useState<string | null>(null);
-  const [currentPage, setCurrentPage] = useState(() => {
-    const savedPage = sessionStorage.getItem('currentPage');
-    return savedPage ? JSON.parse(savedPage) : 1;
-  });
-  const gamesPerPage = 30; // Numero di giochi per pagina
-  const [selectedAppId, setSelectedAppId] = useState<number | null>(null); // Per l'appid selezionato
-  const navigate = useNavigate();
-  const [searchTerm, setSearchTerm] = useState('');
+	const [games, setGames] = useState<OwnedGame[]>([]);
+	const [error, setError] = useState<string | null>(null);
+	const [currentPage, setCurrentPage] = useState(() => {
+		const savedPage = sessionStorage.getItem("currentPage");
+		return savedPage ? JSON.parse(savedPage) : 1;
+	});
+	const gamesPerPage = 30; // Numero di giochi per pagina
+	const [selectedAppId, setSelectedAppId] = useState<number | null>(null); // Per l'appid selezionato
+	const navigate = useNavigate();
+	const [searchTerm, setSearchTerm] = useState("");
 
-  useEffect(() => {
+	useEffect(() => {
+		// Titolo della pagina
+		document.title = "I miei giochi";
 
-    // Titolo della pagina
-    document.title = 'I miei giochi';
+		sessionStorage.setItem("currentPage", JSON.stringify(currentPage));
 
-    sessionStorage.setItem('currentPage', JSON.stringify(currentPage));
+		const fetchOwnedGames = async () => {
+			try {
+				// Così sono nascoste le key
+				const apiKey = import.meta.env.VITE_STEAM_API_KEY;
+				const steamId = import.meta.env.VITE_STEAM_ID;
 
-    const fetchOwnedGames = async () => {
-      try {
-        // Così sono nascoste le key
-        const apiKey = import.meta.env.VITE_STEAM_API_KEY;
-        const steamId = import.meta.env.VITE_STEAM_ID;
+				const response = await fetch(
+					`/api/IPlayerService/GetOwnedGames/v0001/?key=${apiKey}&steamid=${steamId}&format=json&include_appinfo=true`
+				);
+				// console.log("api", apiKey);
 
+				if (!response.ok) {
+					throw new Error(`HTTP error! status: ${response.status}`);
+				}
 
-        const response = await fetch(`/api/IPlayerService/GetOwnedGames/v0001/?key=${apiKey}&steamid=${steamId}&format=json&include_appinfo=true`);
-        // console.log("api", apiKey);
+				const data = await response.json();
+				console.log(data);
+				const ownedGames: OwnedGame[] = data.response.games;
 
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
+				// Ordina i giochi per ore giocate
+				ownedGames.sort((a, b) => b.playtime_forever - a.playtime_forever);
 
-        const data = await response.json();
-        console.log(data)
-        const ownedGames: OwnedGame[] = data.response.games;
+				// Aggiorna lo stato con i giochi posseduti
+				setGames(ownedGames);
+			} catch (error) {
+				if (error instanceof Error) {
+					setError(error.message);
+				} else {
+					setError("Si è verificato un errore sconosciuto");
+				}
+			}
+		};
 
-        // Ordina i giochi per ore giocate
-        ownedGames.sort((a, b) => b.playtime_forever - a.playtime_forever);
+		fetchOwnedGames();
+	}, [currentPage]);
 
-        // Aggiorna lo stato con i giochi posseduti
-        setGames(ownedGames);
-      } catch (error) {
-        if (error instanceof Error) {
-          setError(error.message);
-        } else {
-          setError('Si è verificato un errore sconosciuto');
-        }
-      }
-    };
+	const filteredGames = games.filter((game) =>
+		game.name.toLowerCase().includes(searchTerm.toLowerCase())
+	);
 
-    fetchOwnedGames();
-  }, [currentPage]);
+	useEffect(() => {
+		window.scrollTo({ top: 0, behavior: "smooth" });
+	}, [currentPage]);
 
-  const filteredGames = games.filter(game =>
-    game.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+	const timeSinceLastPlayed = (timestamp: number) => {
+		const lastPlayedDate = new Date(timestamp * 1000); // Converti in millisecondi
+		const now = new Date();
+		const differenceInMillis = now.getTime() - lastPlayedDate.getTime(); // Calcola la differenza in millisecondi
 
-  useEffect(() => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  }, [currentPage]);
+		const differenceInMinutes = Math.floor(differenceInMillis / (1000 * 60));
+		const differenceInHours = Math.floor(differenceInMinutes / 60);
+		const differenceInDays = Math.floor(differenceInHours / 24);
 
+		let timeAgo: string;
+		if (differenceInDays > 0) {
+			timeAgo = `${differenceInDays} giorni fa`;
+		} else if (differenceInHours > 0) {
+			timeAgo = `${differenceInHours} ore fa`;
+		} else {
+			timeAgo = `${differenceInMinutes} minuti fa`;
+		}
 
-  const timeSinceLastPlayed = (timestamp: number) => {
-    const lastPlayedDate = new Date(timestamp * 1000); // Converti in millisecondi
-    const now = new Date();
-    const differenceInMillis = now.getTime() - lastPlayedDate.getTime(); // Calcola la differenza in millisecondi
+		// Formatta la data in una stringa leggibile
+		const formattedDate = lastPlayedDate.toLocaleDateString("it-IT", {
+			year: "numeric",
+			month: "long",
+			day: "numeric",
+		});
 
-    const differenceInMinutes = Math.floor(differenceInMillis / (1000 * 60));
-    const differenceInHours = Math.floor(differenceInMinutes / 60);
-    const differenceInDays = Math.floor(differenceInHours / 24);
+		return `${timeAgo} (${formattedDate})`; // Restituisce il messaggio formattato
+	};
 
-    let timeAgo: string;
-    if (differenceInDays > 0) {
-      timeAgo = `${differenceInDays} giorni fa`;
-    } else if (differenceInHours > 0) {
-      timeAgo = `${differenceInHours} ore fa`;
-    } else {
-      timeAgo = `${differenceInMinutes} minuti fa`;
-    }
+	if (error) {
+		return <div className="lg:container mx-auto">Errore: {error}</div>;
+	}
 
-    // Formatta la data in una stringa leggibile
-    const formattedDate = lastPlayedDate.toLocaleDateString('it-IT', {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-    });
+	// Calcola i giochi da mostrare in base alla pagina corrente
+	const indexOfLastGame = currentPage * gamesPerPage;
+	const indexOfFirstGame = indexOfLastGame - gamesPerPage;
+	// const currentGames = games.slice(indexOfFirstGame, indexOfLastGame);
 
-    return `${timeAgo} (${formattedDate})`; // Restituisce il messaggio formattato
-  };
+	const currentGames = filteredGames.slice(indexOfFirstGame, indexOfLastGame);
 
-  if (error) {
-    return <div className="lg:container mx-auto">Errore: {error}</div>;
-  }
+	// Calcola il numero totale di pagine
+	// const totalPages = Math.ceil(games.length / gamesPerPage);
 
-  // Calcola i giochi da mostrare in base alla pagina corrente
-  const indexOfLastGame = currentPage * gamesPerPage;
-  const indexOfFirstGame = indexOfLastGame - gamesPerPage;
-  // const currentGames = games.slice(indexOfFirstGame, indexOfLastGame);
+	const totalPages = Math.ceil(filteredGames.length / gamesPerPage);
 
-  const currentGames = filteredGames.slice(indexOfFirstGame, indexOfLastGame);
+	const handleGameClick = (
+		appid: number,
+		name: string,
+		currentPage: number
+	) => {
+		setSelectedAppId(appid);
+		navigate(`/news/${appid}/${encodeURIComponent(name)}?page=${currentPage}`);
+	};
 
-  // Calcola il numero totale di pagine
-  // const totalPages = Math.ceil(games.length / gamesPerPage);
+	return (
+		<div className="lg:container mx-auto pt-8">
+			<h1 className="mb-4 font-bold">Giochi Posseduti</h1>
 
-  const totalPages = Math.ceil(filteredGames.length / gamesPerPage);
+			<input
+				type="text"
+				placeholder="Cerca giochi..."
+				value={searchTerm}
+				onChange={(e) => setSearchTerm(e.target.value)}
+				className="mb-4 p-2 rounded border border-gray-300 focus:border-blue-500 focus:shadow-outline transition ease-in-out"
+			/>
 
-  const handleGameClick = (appid: number, name: string, currentPage: number) => {
-    setSelectedAppId(appid);
-    navigate(`/news/${appid}/${encodeURIComponent(name)}?page=${currentPage}`);
-  };
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 transition ease-in-out ">
+				{currentGames.map((game) => (
+					<div key={game.appid} className="flex flex-col p-4 rounded-lg">
+						<h2 className="text-lg font-bold flex-grow">{game.name}</h2>
 
+						<div className="group relative">
+							<img
+								src={`https://media.steampowered.com/steam/apps/${game.appid}/header.jpg`}
+								alt={game.name}
+								className="w-full h-auto rounded transition ease-in-out hover:scale-110"
+							/>
 
-  return (
-    <div className='lg:container mx-auto pt-8'>
-      <h1 className='mb-4 font-bold'>Giochi Posseduti</h1>
+							{/* Container per le icone grazie al group-hover posso cambiare l'opacità delle icone */}
+							<div className="opacity-0 absolute inset-0 flex items-center justify-center group-hover:opacity-100 transition-opacity duration-300 bgIcons">
+								<a
+									href={`https://store.steampowered.com/app/${game.appid}`}
+									target="_blank"
+									rel="noopener noreferrer"
+									onClick={(e) => {
+										e.stopPropagation(); // Previene la propagazione dell'evento
+									}}
+									className="flex items-center justify-center text-4xl iconOnHover text-white bg-black p-2 rounded-full mx-2 hover:text-white hover:scale-150 transition ease-in-out p-12 hover:border iconShadow"
+								>
+									<i className="fa-brands fa-steam"></i>
+								</a>
 
-      <input
-        type="text"
-        placeholder="Cerca giochi..."
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        className="mb-4 p-2 rounded border border-gray-300 focus:border-blue-500 focus:shadow-outline transition ease-in-out"
-      />
+								<a
+									onClick={() =>
+										handleGameClick(game.appid, game.name, currentPage)
+									}
+									className="flex items-center justify-center text-4xl iconOnHover text-white bg-black p-2 rounded-full mx-2 cursor-pointer hover:text-white hover:scale-150 transition ease-in-out p-12 hover:border iconShadow"
+								>
+									<i className="fas fa-newspaper"></i> {/* Icona Notizie */}
+								</a>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 transition ease-in-out ">
-        {currentGames.map((game) => (
-          <div key={game.appid} className="flex flex-col p-4 rounded-lg">
-            <h2 className="text-lg font-bold flex-grow">{game.name}</h2>
+								<a
+									onClick={(e) => {
+										e.stopPropagation();
+										navigate(
+											`/achievements/${game.appid}/${encodeURIComponent(
+												game.name
+											)}?page=${currentPage}`
+										);
+									}}
+									className="flex items-center justify-center text-4xl iconOnHover text-white bg-black p-2 rounded-full mx-2 cursor-pointer hover:text-white hover:scale-150 transition ease-in-out p-12 hover:border iconShadow"
+								>
+									<i className="fas fa-trophy"></i> {/* Icona Obiettivi */}
+								</a>
+							</div>
+						</div>
 
+						<p>Ore giocate: {Math.floor(game.playtime_forever / 60)} ore</p>
+						{game.playtime_forever > 0 ? (
+							<p>Ultimo avvio: {timeSinceLastPlayed(game.rtime_last_played)}</p>
+						) : null}
+					</div>
+				))}
+			</div>
 
-            <div className="group relative">
-              <img
-                src={`https://media.steampowered.com/steam/apps/${game.appid}/header.jpg`}
-                alt={game.name}
-                className="w-full h-auto rounded transition ease-in-out hover:scale-110"
-              />
-
-              {/* Container per le icone grazie al group-hover posso cambiare l'opacità delle icone */}
-              <div className="opacity-0 absolute inset-0 flex items-center justify-center group-hover:opacity-100 transition-opacity duration-300 bgIcons">
-                <a
-                  href={`https://store.steampowered.com/app/${game.appid}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  onClick={(e) => {
-                    e.stopPropagation(); // Previene la propagazione dell'evento
-                  }}
-                  className="flex items-center justify-center text-4xl iconOnHover text-white bg-black p-2 rounded-full mx-2 hover:text-white hover:scale-150 transition ease-in-out p-12 hover:border iconShadow"
-                >
-                  <i className="fa-brands fa-steam"></i>
-                </a>
-
-
-                <a
-                  onClick={() => handleGameClick(game.appid, game.name, currentPage)}
-                  className="flex items-center justify-center text-4xl iconOnHover text-white bg-black p-2 rounded-full mx-2 cursor-pointer hover:text-white hover:scale-150 transition ease-in-out p-12 hover:border iconShadow"
-                >
-                  <i className="fas fa-newspaper"></i> {/* Icona Notizie */}
-                </a>
-
-
-                <a
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    navigate(`/achievements/${game.appid}/${encodeURIComponent(game.name)}?page=${currentPage}`);
-                  }}
-                  className="flex items-center justify-center text-4xl iconOnHover text-white bg-black p-2 rounded-full mx-2 cursor-pointer hover:text-white hover:scale-150 transition ease-in-out p-12 hover:border iconShadow"
-                >
-                  <i className="fas fa-trophy"></i> {/* Icona Obiettivi */}
-                </a>
-
-
-              </div>
-            </div>
-
-
-            <p>Ore giocate: {Math.floor(game.playtime_forever / 60)} ore</p>
-            {game.playtime_forever > 0 ? (
-              <p>Ultimo avvio: {timeSinceLastPlayed(game.rtime_last_played)}</p>
-            ) : null}
-          </div>
-        ))}
-      </div>
-
-      <div className="flex justify-center mt-4">
-        {totalPages > 1 && Array.from({ length: totalPages }, (_, index) => (
-          <button
-            key={index + 1}
-            className={`px-4 py-2 mx-1 border ${index + 1 === currentPage ? 'bg-gray-800 text-white' : 'bg-white text-gray-800'}`}
-            onClick={() => setCurrentPage(index + 1)}
-          >
-            {index + 1}
-          </button>
-        ))}
-      </div>
-    </div>
-  );
-}
+			<div className="flex justify-center mt-4 pb-8">
+				{totalPages > 1 &&
+					Array.from({ length: totalPages }, (_, index) => (
+						<button
+							key={index + 1}
+							className={`px-4 py-2 mx-1 border ${
+								index + 1 === currentPage
+									? "bg-gray-800 text-white"
+									: "bg-white text-gray-800"
+							}`}
+							onClick={() => setCurrentPage(index + 1)}
+						>
+							{index + 1}
+						</button>
+					))}
+			</div>
+		</div>
+	);
+};
 
 export default GameFeed;
